85dded4ccd299135fa4935793952b028b2939110
con_mame <con_mame@conma.me>
2012-09-25 19:46:11 +0900

    Remove unuse function

--- dev_setup/cookbooks/postgresql/recipes/service.rb
+++ dev_setup/cookbooks/postgresql/recipes/service.rb
@@ -19,7 +19,6 @@ else
     Chef::Log.error("Installation of PostgreSQL is not supported on this platform.")
 end
 
-cf_pg_install(pg_major_version, pg_port)
 cf_pg_update_hba_conf(node[:postgresql_node][:database], node[:postgresql][:server_root_user], node[:postgresql][:service_version])
 cf_pg_hba_local_trust(node[:postgresql][:service_version])
 cf_pg_setup_db(node[:postgresql_node][:database], node[:postgresql][:server_root_user], node[:postgresql][:server_root_password], true, node[:postgresql][:service_port])
