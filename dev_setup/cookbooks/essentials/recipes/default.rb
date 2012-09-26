da8554a0f5a9a0cf6484e3c017e413bd4b06693b
con_mame <con_mame@conma.me>
2012-09-26 10:01:12 +0900

    Add centos essensial packages

--- dev_setup/cookbooks/essentials/recipes/default.rb
+++ dev_setup/cookbooks/essentials/recipes/default.rb
@@ -6,39 +6,51 @@
 #
 #
 
-%w{apt-utils build-essential libssl-dev
-   libxml2 libxml2-dev libxslt1.1 libxslt1-dev git-core sqlite3 libsqlite3-ruby
-   libsqlite3-dev unzip zip ruby-dev libmysql-ruby libmysqlclient-dev libcurl4-openssl-dev}.each do |p|
-  package p do
-    action [:install]
+case node['platform']
+when "ubuntu"
+  %w{apt-utils build-essential libssl-dev
+     libxml2 libxml2-dev libxslt1.1 libxslt1-dev git-core sqlite3 libsqlite3-ruby
+     libsqlite3-dev unzip zip ruby-dev libmysql-ruby libmysqlclient-dev libcurl4-openssl-dev}.each do |p|
+    package p do
+      action [:install]
+    end
   end
-end
 
-machine =  node[:kernel][:machine]
-libpq_deb_path = File.join(node[:deployment][:setup_cache], "libpq5_9.2.deb")
-cf_remote_file libpq_deb_path do
-  owner node[:deployment][:user]
-  id node[:postgresql][:id][:libpq]["#{machine}"]
-  checksum node[:postgresql][:checksum][:libpq]["#{machine}"]
-end
+  machine =  node[:kernel][:machine]
+  libpq_deb_path = File.join(node[:deployment][:setup_cache], "libpq5_9.2.deb")
+  cf_remote_file libpq_deb_path do
+    owner node[:deployment][:user]
+    id node[:postgresql][:id][:libpq]["#{machine}"]
+    checksum node[:postgresql][:checksum][:libpq]["#{machine}"]
+  end
 
-libpq_dev_deb_path = File.join(node[:deployment][:setup_cache], "libpq-dev_9.2.deb")
-cf_remote_file libpq_dev_deb_path do
-  owner node[:deployment][:user]
-  id node[:postgresql][:id][:libpq_dev]["#{machine}"]
-  checksum node[:postgresql][:checksum][:libpq_dev]["#{machine}"]
-end
+  libpq_dev_deb_path = File.join(node[:deployment][:setup_cache], "libpq-dev_9.2.deb")
+  cf_remote_file libpq_dev_deb_path do
+    owner node[:deployment][:user]
+    id node[:postgresql][:id][:libpq_dev]["#{machine}"]
+    checksum node[:postgresql][:checksum][:libpq_dev]["#{machine}"]
+  end
 
-bash "Install libpq" do
-  code <<-EOH
-  dpkg -i #{libpq_deb_path}
-  EOH
-end
+  bash "Install libpq" do
+    code <<-EOH
+    dpkg -i #{libpq_deb_path}
+    EOH
+  end
 
-bash "Install libpq-dev" do
-  code <<-EOH
-  dpkg -i #{libpq_dev_deb_path}
-  EOH
+  bash "Install libpq-dev" do
+    code <<-EOH
+    dpkg -i #{libpq_dev_deb_path}
+    EOH
+  end
+
+when "centos"
+  %w{openssl-devel
+     libxml2 libxml2-devel libxslt libxslt-devel git-core sqlite ruby-sqlite3
+     sqlite-devel unzip zip ruby-devel ruby-mysql mysql-devel libcurl-devel postgresql-libs}.each do |p|
+    package p do
+      action [:install]
+    end
+  end
 end
 
 if node[:deployment][:profile]
